cea53b2b01c11efcee482788118a8e4132d8be33
Aditya Patel <aditya.patel1616@gmail.com>
2023-07-12 09:17:44 -0500

    Adding files for hw2

---
+++ hw2/hw2.py
@@ -0,0 +1,70 @@
+import sys
+
+# Use this file to write your queries. Submit this file in Brightspace after finishing your homework.
+
+#TODO: Write your username and answer to each query as a string in the return statements in the functions below. 
+# Do not change the function names. 
+
+#Your resulting tables should have the attributes in the same order as appeared in the sample answers. 
+
+#Make sure to test that python prints out the strings (your username and SQL queries) correctly.
+
+#usage: python hw1.py or python3 hw1.py
+
+def username():
+	return "pate1854"
+    
+def query1():
+	return """
+    SELECT * 
+    FROM Place
+    WHERE price < 100;
+	"""
+
+def query2():
+	return "2"
+
+def query3():
+	return "3"
+	
+def query4():
+	return "4"
+
+def query5():
+	return "5"
+
+def query6():
+	return "6"
+
+def query7():
+	return "7"
+
+def query8():
+	return "8"
+
+def query9():
+	return "9"
+
+#Do not edit below
+
+def main():
+	query_options = {1: query1(), 2: query2(), 3: query3(), 4: query4(), 5: query5(), 6: query6(), 7: query7(), 8: query8(), 
+		9: query9()}
+	
+	if len(sys.argv) == 1:
+		if username() == "username":
+			print("Make sure to change the username function to return your username.")
+			return
+		else:
+			print(username())
+		for query in query_options.values():
+			print(query)
+	elif len(sys.argv) == 2:
+		if sys.argv[1] == "username":
+			print(username())
+		else:
+			print(query_options[int(sys.argv[1])])
+
+	
+if __name__ == "__main__":
+   main()

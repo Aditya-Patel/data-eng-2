828346c1e52690e8f2a09937bd4944b0182991ed
Aditya Patel <aditya.patel1616@gmail.com>
2023-08-03 21:38:33 -0500

    Solved through b

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -5,9 +5,8 @@ Homework 5
 """
 
 import pandas as pd
-import math
 import sqlite3 as sql
-import csv
+import sklearn.cluster as sklc
 
 # Connect to database using sqlite3
 con = sql.connect("chinook.db")
@@ -15,7 +14,7 @@ con = sql.connect("chinook.db")
 # a) Write an SQL query to gather information about the artists, their albums, tracks, genres, and playlists. Import this information into a pandas data frame.
 
 query = '''
-    SELECT r.ArtistId, r.Name, l.AlbumId, l.Title, t.TrackId, t.Name, g.GenreId, g.Name, p.PlaylistId, p.Name
+    SELECT r.ArtistId, r.Name as Artist, l.AlbumId, l.Title as Album, t.TrackId, t.Name, g.GenreId, g.Name as Genre, p.PlaylistId, p.Name as Playlist
     FROM Track t
     JOIN Album l USING(AlbumId)
     JOIN Artist r USING(ArtistId)
@@ -24,6 +23,18 @@ query = '''
     JOIN Playlist p USING(PlaylistId);
     '''
 
-join_df = pd.read_sql_query(query, con)
+chinook_df = pd.read_sql_query(query, con)
 
-print(join_df.head)
+# b) Select all the artists that have more than one album for the analysis below
+artist_df = chinook_df.groupby(['ArtistId']).filter(lambda x: x['AlbumId'].nunique() > 1)
+
+# c) Construct a set of ten features for each artist
+# * Genres: The first seven features represent the seven most popular genres. Create a numerical feature for each of the top 7 genres that records how many songs an artist has in each genre. (Determine the top 7 genres based on the number of tracks in each genre.)
+# * Number of albums: Count of how many albums the artist has in the data (note this should be > 1 based on filter above).
+# * Number of tracks: Count of how many tracks each artist has.
+# * Number of playlists: Number of playlists that include tracks of an artist
+
+# Get counts from artist information
+artistCounts_df = artist_df.groupby(['ArtistId', 'Artist']).agg({'AlbumId': 'nunique', 'TrackId': 'nunique', 'PlaylistId': 'nunique'}).reset_index()
+
+# Create Genre Information
